92c1035318868a8975412e0412e2c81760c3554c
timtielo <tim@tielo-digital.nl>
2025-10-20 13:03:35 +0200

    Updated WebsitesPage.tsx

--- src/pages/ServicePage/WebsitesPage.tsx
+++ src/pages/ServicePage/WebsitesPage.tsx
@@ -3,6 +3,7 @@ import { ContentfulSEO } from '../../components/SEO/ContentfulSEO';
 import { WebsitesHero } from './WebsitesPage/components/WebsitesHero';
 import { WebsitesValue } from './WebsitesPage/components/WebsitesValue';
 import { WebsitesOffer } from './WebsitesPage/components/WebsitesOffer';
+import { WebsitesShowcase } from './WebsitesPage/components/WebsitesShowcase';
 import { WebsitesPortfolio } from './WebsitesPage/components/WebsitesPortfolio';
 import { WebsitesProcess } from './WebsitesPage/components/WebsitesProcess';
 import { WebsitesForm } from './WebsitesPage/components/WebsitesForm';
@@ -20,6 +21,7 @@ export function WebsitesPage() {
       <WebsitesHero />
       <WebsitesValue />
       <WebsitesOffer />
+      <WebsitesShowcase />
       <WebsitesPortfolio />
       <WebsitesProcess />
       <WebsitesForm />
