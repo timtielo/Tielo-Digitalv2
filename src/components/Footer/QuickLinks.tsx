a11f0b00c25995ff7cb829a0c2071df776a05c88
timtielo <tim@tielo-digital.nl>
2025-11-14 21:38:26 +0100

    Updated QuickLinks.tsx

--- src/components/Footer/QuickLinks.tsx
+++ src/components/Footer/QuickLinks.tsx
@@ -9,6 +9,7 @@ export function QuickLinks() {
         <li><Link href="/oplossingen" className="text-white/80 hover:text-white">Oplossingen</Link></li>
         <li><Link href="/diensten/websites" className="text-white/80 hover:text-white">Websites</Link></li>
         <li><Link href="/diensten" className="text-white/80 hover:text-white">Alle Diensten</Link></li>
+        <li><Link href="/blog" className="text-white/80 hover:text-white">Blog</Link></li>
         <li><Link href="/contact" className="text-white/80 hover:text-white">Contact</Link></li>
         <li><Link href="/contact" className="text-white/80 hover:text-white">Gratis Opzetje</Link></li>
       </ul>
