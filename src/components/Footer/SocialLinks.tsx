e44e600b091c7190516bd9e281e533837948af49
timtielo <tim@tielo-digital.nl>
2026-02-16 21:39:27 +0100

    Updated SocialLinks.tsx

--- src/components/Footer/SocialLinks.tsx
+++ src/components/Footer/SocialLinks.tsx
@@ -1,16 +1,5 @@
 import React from 'react';
-import { Mail } from 'lucide-react';
 
 export function SocialLinks() {
-  return (
-    <div className="flex space-x-4 mt-4">
-      <a
-        href="mailto:tim@tielo-digital.nl"
-        className="text-white/70 hover:text-tielo-orange transition-colors"
-        aria-label="Email"
-      >
-        <Mail className="w-6 h-6" />
-      </a>
-    </div>
-  );
+  return null;
 }
